9d2b90bebd2ffef2726772fcbdf979176dd6a0bb
Thomas <thomasjamestiamlee@gmail.com>
2017-06-08 18:14:58 +0900

    Changed background color to white.

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 function love.load()
 	gameSheet = love.graphics.newImage("assets/game.png")
+	love.graphics.setBackgroundColor(255, 255, 255)
 end
 
 function love.draw()
+	
 	love.graphics.draw(gameSheet, 0, 0)
 end
