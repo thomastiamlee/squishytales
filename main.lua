2d83b1da91d4406ce1719fe4ab77517c7484f51a
Thomas <thomasjamestiamlee@gmail.com>
2017-06-08 18:30:27 +0900

    Drawed Suishy quad and changed position to center of the window.

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 function love.load()
 	gameSheet = love.graphics.newImage("assets/game.png")
+	screenWidth = love.graphics.getWidth()
+	screenHeight = love.graphics.getHeight()
 	love.graphics.setBackgroundColor(255, 255, 255)
 end
 
 function love.draw()
-	
-	love.graphics.draw(gameSheet, 0, 0)
+	quad = love.graphics.newQuad(0, 0, 75, 75, gameSheet:getWidth(), gameSheet:getHeight())
+	love.graphics.draw(gameSheet, quad, screenWidth / 2, screenHeight / 2 + 15, 0, 0.75, 0.75, 37, 37)
 end
