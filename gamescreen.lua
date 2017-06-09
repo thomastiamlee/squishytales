fe2887229fcc32dfedf449467182065097ca9e8b
Thomas James Tiam-Lee <thomasjamestiamlee@gmail.com>
2017-06-10 00:53:05 +0800

    Made game element sizes responsive.

--- gamescreen.lua
+++ gamescreen.lua
@@ -1,8 +1,14 @@
 GameScreen = {
+	-- Squishy
 	squishy = nil,
+	squishyScale = nil,
+	-- Bubble
 	bubble = nil,
+	bubblePop = nil,
 	bubbleLoc = nil,
 	bubblestatus = nil,
+	bubbleScale = nil,
+	-- Area data (level specific information)
 	areadata = nil,
 	new = function(self)
 		o = {}
@@ -12,7 +18,10 @@ GameScreen = {
 	end,
 	init = function(self, data)
 		self.squishy = MSprite:new(gameSheetImage, 75, 75, 0, 0, 7, 1, 100)
+		self.squishyScale = screenHeight * 0.1 / 75
 		self.bubble = MSprite:new(gameSheetImage, 225, 225, 0, 525, 1, 1, 0)
+		self.bubbleScale = screenHeight * 0.25 / 225
+		self.bubblePop = MSprite:new(gameSheetImage, 225, 225, 0, 750, 1, 1, 0)
 		self.areadata = data.areadata
 		self.bubbledata = false
 	end,
@@ -21,9 +30,9 @@ GameScreen = {
 	end,
 	draw = function(self)
 		self.areadata:drawbackground()
-		self.squishy:drawcenter(screenWidth / 2, screenHeight / 2, 0, 0.75)
+		self.squishy:drawcenter(screenWidth / 2, screenHeight / 2, 0, self.squishyScale)
 		if self.bubbledata then
-			self.bubble:drawcenter(self.bubbleLoc.x, self.bubbleLoc.y)
+			self.bubble:drawcenter(self.bubbleLoc.x, self.bubbleLoc.y, 0, self.bubbleScale)
 		end
 	end,
 	mousepressed = function(self, x, y)
