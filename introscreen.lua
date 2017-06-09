a133a67e92c5b85d18ea9af5d134445c2a3bb03b
Thomas <thomasjamestiamlee@gmail.com>
2017-06-09 18:07:46 +0900

    Added bubbles for intro and changed screen resolution to mobile.

--- introscreen.lua
+++ introscreen.lua
@@ -18,7 +18,10 @@ IntroScreen = {
 	draw = function(self)
 		love.graphics.setColor(255, 255, 255)
 		self.menuBg:draw(0, 0, 0, self.bgScale)
-		self.excelsiorLogo:drawcenter(screenWidth / 2 - 150, screenHeight / 2, 0, 0.3)
-		self.loveLogo:drawcenter(screenWidth / 2 + 150, screenHeight / 2, 0, 0.5)
+		self.excelsiorLogo:drawcenter(screenWidth / 2 - 150, screenHeight / 2, 0, 1)
+		self.loveLogo:drawcenter(screenWidth / 2 + 150, screenHeight / 2, 0, 1)
+		love.graphics.setFont(quicksandFont)
+		love.graphics.printf("CREATED BY", screenWidth / 2 - 250, screenHeight / 2 - loveLogoImage:getHeight() * 0.6, 200, "center")
+		love.graphics.printf("POWERED BY", screenWidth / 2 + 50, screenHeight / 2 - loveLogoImage:getHeight() * 0.6, 200, "center")
 	end
 }
--- loader.lua
+++ loader.lua
@@ -1,17 +1,18 @@
 function loadResources()
 	-- Sheets
 	gameSheetImage = love.graphics.newImage("assets/game.png")
-	loveLogoImage = love.graphics.newImage("assets/love.png")
-	excelsiorLogoImage = love.graphics.newImage("assets/excelsior.png")
+	loveLogoImage = love.graphics.newImage("assets/love-bubble.png")
+	excelsiorLogoImage = love.graphics.newImage("assets/excelsior-bubble.png")
 	menuBgImage = love.graphics.newImage("assets/menuback.png")
 	-- Fonts
 	quicksandFont = love.graphics.newFont("assets/quicksand.otf", 24)
 end
 
 function initGame()
+	-- Background settings
+	love.window.setMode(800, 480)
+	love.graphics.setBackgroundColor(255, 255, 255)
 	-- Global variables
 	screenWidth = love.graphics.getWidth()
 	screenHeight = love.graphics.getHeight()
-	-- Background settings
-	love.graphics.setBackgroundColor(255, 255, 255)
 end
