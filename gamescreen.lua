06896acb6939061dafddd0a808668bae36f442bc
Thomas James Tiam-Lee <thomasjamestiamlee@gmail.com>
2017-06-10 15:35:46 +0800

    Refactored Squishy and bubble elements codes.

--- gamescreen.lua
+++ gamescreen.lua
@@ -1,15 +1,6 @@
 GameScreen = {
-	-- Squishy
 	squishy = nil,
-	squishyScale = nil,
-	-- Bubble
 	bubble = nil,
-	bubblePop = nil,
-	bubbleLoc = nil,
-	bubbledata = nil,
-	bubbleGrowScale = nil,
-	bubbleGrowSpeed = 0.025,
-	bubbleScale = nil,
 	-- Area data (level specific information)
 	areadata = nil,
 	new = function(self)
@@ -19,52 +10,90 @@ GameScreen = {
 		return o
 	end,
 	init = function(self, data)
-		self.squishy = MSprite:new(gameSheetImage, 75, 75, 0, 0, 7, 1, 100)
-		self.squishyScale = screenHeight * 0.1 / 75
-		self.bubble = MSprite:new(gameSheetImage, 225, 225, 0, 525, 1, 1, 0)
-		self.bubbleScale = screenHeight * 0.25 / 225
-		self.bubblePop = MSprite:new(gameSheetImage, 225, 225, 0, 750, 1, 1, 0)
+		self.squishy = {
+			image = MSprite:new(gameSheetImage, 75, 75, 0, 0, 7, 1, 100),
+			scale = screenHeight * 0.1 / 75,
+			x = screenWidth / 2,
+			y = screenHeight / 2,
+			vx = 0,
+			vy = 0,
+			update = function(self)
+				self.image:update()
+				self.x = self.x + self.vx
+				self.y = self.y + self.vy
+			end,
+			draw = function(self)
+				self.image:drawcenter(self.x, self.y, 0, self.scale)
+			end
+		}
+		self.bubble = {
+			image = MSprite:new(gameSheetImage, 225, 225, 0, 525, 1, 1, 0),
+			popimage = MSprite:new(gameSheetImage, 225, 225, 0, 750, 1, 1, 0),
+			x = 0,
+			y = 0,
+			popduration = 75,
+			currpopduration = 0,
+			scale = screenHeight * 0.25 / 225,
+			growscale = 0,
+			growspeed = 0.03,
+			status = "none",
+			spawn = function(self)
+				self.status = "grow"
+				self.growscale = 0.25
+				self.currpopduration = 0
+			end,
+			pop = function(self, callback)
+				self.status = "pop"
+				callback()
+			end,
+			update = function(self)
+				if self.status == "grow" then
+					self.growscale = self.growscale + self.growspeed					
+					if self.growscale >= 1.25 then
+						self:pop(function() end)
+					end
+				elseif self.status == "pop" then
+					self.currpopduration = self.currpopduration + elapsedTime * 1000
+					if self.currpopduration >= self.popduration then
+						self.status = "none"
+					end
+				end
+			end,
+			draw = function(self)
+				if self.status == "grow" then
+					self.image:drawcenter(self.x, self.y, 0, self.scale * self.growscale)
+				elseif self.status == "pop" then
+					self.popimage:drawcenter(self.x, self.y, 0, self.scale * self.growscale * 1.75)
+				end
+			end
+		}
 		self.areadata = data.areadata
-		self.bubbledata = false
 	end,
 	update = function(self)
 		self.squishy:update()
-		if self.bubbledata then
-			self.bubbleGrowScale = self.bubbleGrowSpeed + self.bubbleGrowScale
-			print(self.bubbleGrowScale)
-			if self.bubbleGrowScale > 1.2 then
-				self.bubbledata = false
-			end
-		end
+		self.bubble:update()
 	end,
 	draw = function(self)
 		self.areadata:drawbackground()
-		self.squishy:drawcenter(screenWidth / 2, screenHeight / 2, 0, self.squishyScale)
-		if self.bubbledata then
-			if self.bubbleGrowScale <= 1 then
-				self.bubble:drawcenter(self.bubbleLoc.x, self.bubbleLoc.y, 0, self.bubbleScale * self.bubbleGrowScale)
-			else
-				self.bubblePop:drawcenter(self.bubbleLoc.x, self.bubbleLoc.y, 0, self.bubbleScale)
-			end
-		end
+		self.squishy:draw()
+		self.bubble:draw()
 	end,
 	mousepressed = function(self, x, y)
-		if self.bubbledata == false then
-			self.bubbledata = true
-			self.bubbleGrowScale = 0
-			self.bubbleLoc = {x = x, y = y}
+		if self.bubble.status == "none" then
+			self.bubble:spawn()
+			self.bubble.x = x
+			self.bubble.y = y
 		end
 	end,
 	mousereleased = function(self, x, y)
-		if self.bubbledata == true then
-			self.bubbledata = false
+		if self.bubble.status == "grow" then
+			self.bubble:pop(function() end)
 		end
 	end,
 	mousemoved = function(self, x, y)
-		if self.bubbledata == true then
-			if self.bubbleGrowScale <= 1 then
-				self.bubbleLoc = {x = x, y = y}
-			end
+		if self.bubble.status == "grow" then
+			self.bubble.x = x
+			self.bubble.y = y
 		end
 	end
 }
